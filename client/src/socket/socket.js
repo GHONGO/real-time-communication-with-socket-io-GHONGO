ee2d30ce2b8c89df1f6bc3e9eb5730fd285b5d80
GHONGO <griffinwhongo@gmail.com>
2025-11-02 18:11:06 +0300

    Update SOCKET_URL to production endpoint

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 import { useEffect, useState } from 'react';
 
 // Socket.io connection URL
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
+const SOCKET_URL = 'https://real-time-communication-with-socket-io-jz6z.onrender.com';
 
 // Create socket instance
 export const socket = io(SOCKET_URL, {
@@ -147,4 +147,4 @@ export const useSocket = () => {
   };
 };
 
-export default socket; 
+export default socket;
